9ff4e4ca3b6fb3ca87739de5e3277c399113abd4
xyh123321 <376564838@qq.com>
2020-07-25 11:53:20 +0800

    [parking lot][jayden] feature:pass should_return_no_positon_when_park_given_car_pakingboy_2_lots_all_noposition

--- src/main/java/com/oocl/cultivation/ParkingBoy.java
+++ src/main/java/com/oocl/cultivation/ParkingBoy.java
@@ -43,7 +43,7 @@ public class ParkingBoy {
                 return this.parkingLotList.get(i).getLotName()+":"+car.getCarId();
             }
         }
-        return "";
+        return "Not enough position.";
     }
 
     public String fetchCar(Ticket ticket) {
